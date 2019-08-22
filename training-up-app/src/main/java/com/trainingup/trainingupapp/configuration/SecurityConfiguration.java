73978ba4c5a7e74d2428c34815404365ce7a94af
Alexandru Georgescu <age@age>
2019-08-22 10:46:09 +0300

    backend v2.1

---
+++ training-up-app/src/main/java/com/trainingup/trainingupapp/configuration/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.trainingup.trainingupapp.configuration;
+
+import com.trainingup.trainingupapp.filter.CustomFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.addFilterBefore(new CustomFilter(), ChannelProcessingFilter.class);
+        httpSecurity.authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .anyRequest()
+                .fullyAuthenticated()
+                .and().httpBasic().and().csrf().disable();
+
+    }
+
+}
