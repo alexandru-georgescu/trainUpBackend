f7a97f6dac9f68569d8f48f88ce234d4b676aaee
Alexandru Georgescu <age@age>
2019-09-09 16:10:29 +0300

    w4 backend d1.8

--- training-up-app/src/main/java/com/trainingup/trainingupapp/convertor/CourseConvertor.java
+++ training-up-app/src/main/java/com/trainingup/trainingupapp/convertor/CourseConvertor.java
@@ -3,22 +3,13 @@ package com.trainingup.trainingupapp.convertor;
 import com.trainingup.trainingupapp.dto.CourseDTO;
 import com.trainingup.trainingupapp.tables.Course;
 
-import java.time.LocalDate;
-
 public class CourseConvertor {
 
     public static Course convertToCourse(CourseDTO course) {
         Course newCourse = new Course();
         newCourse.setProjectManager(course.getProjectManager());
-
-        LocalDate start = course.getStartDate();
-        start.plusDays(1);
-
-        LocalDate stop = course.getStartDate();
-        stop.plusDays(1);
-
-        newCourse.setEndDate(stop);
-        newCourse.setStartDate(stop);
+        newCourse.setEndDate(course.getEndDate());
+        newCourse.setStartDate(course.getStartDate());
         newCourse.setId(course.getId());
         newCourse.setCapacity(course.getCapacity());
         newCourse.setActualCapacity(course.getActualCapacity());
